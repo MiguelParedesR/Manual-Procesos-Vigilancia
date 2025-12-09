221d961c33c05cde23e3a93f09a4952c959016c5
MiguelParedesR <miguelparedesramirez14@gmail.com>
2025-12-09 18:52:53 -0500

    Clarify dist deployment guidance

--- js/index.js
+++
@@ -1,7 +0,0 @@
-function crackAndRedirect(element, local) {
-  element.classList.add("crack-effect");
-  setTimeout(() => {
-    localStorage.setItem("localSeleccionado", local);
-    window.location.href = "identificacion.html";
-  }, 600);
-}
